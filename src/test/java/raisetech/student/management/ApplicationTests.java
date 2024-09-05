a17f9fbfb4b96ecc41de076b34584ed797101833
toyoshima yutaro <yutaro.0905@icloud.com>
2024-09-05 14:19:22 +0900

    スタンダード第３回課題

--- src/test/java/raisetech/student/management/ApplicationTests.java
+++ src/test/java/raisetech/student/management/ApplicationTests.java
@@ -1,13 +1,23 @@
 package raisetech.student.management;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
-class ApplicationTests {
+@RestController
+public class ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		StringUtils.
 	}
 
 }
