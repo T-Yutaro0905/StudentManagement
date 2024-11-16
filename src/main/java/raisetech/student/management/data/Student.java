d00377afbf26af4ea70fb3095de73d1a75828cb7
toyoshima yutaro <yutaro.0905@icloud.com>
2024-11-16 11:41:31 +0900

    第25回演習課題

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,24 +1,36 @@
 package raisetech.student.management.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生")
 @Getter
 @Setter
 public class Student {
 
   private String id;
+
   @NotBlank
   private String name;
+
   @NotBlank
+
   private String kanaName;
+
   private String nickname;
+
   @NotBlank
   private String mailAddress;
+
   private String address;
+
   private int age;
+
   private String gender;
+
   private String remark;
+
   private boolean isDeleted;
 }
